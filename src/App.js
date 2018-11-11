1bb639b71e0edad1388c23f6bca144faf9af211f
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-11 16:16:02 +0200

    Add the login form inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import firebase from 'firebase';
+import { View } from 'react-native';
+import firebase from '@firebase/app';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 	componentWillMount() {
-		firebase.initializeApp({
-			apiKey: 'AIzaSyBe74nyu2lz-9o1H0_CnHmHd0kCOczd_nw',
-			authDomain: 'auth-95faf.firebaseapp.com',
-			databaseURL: 'https://auth-95faf.firebaseio.com',
-			projectId: 'auth-95faf',
-			storageBucket: 'auth-95faf.appspot.com',
-			messagingSenderId: '924318516662'
-		});
+		if (!firebase.apps.length) {
+			firebase.initializeApp({
+				apiKey: 'AIzaSyBe74nyu2lz-9o1H0_CnHmHd0kCOczd_nw',
+				authDomain: 'auth-95faf.firebaseapp.com',
+				databaseURL: 'https://auth-95faf.firebaseio.com',
+				projectId: 'auth-95faf',
+				storageBucket: 'auth-95faf.appspot.com',
+				messagingSenderId: '924318516662'
+			});
+		}
 	}
 
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>An app</Text>
+				<LoginForm />
 			</View>
 		);
 	}
