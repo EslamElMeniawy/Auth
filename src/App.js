c71c6ec43028c1ee2ab031a2f3a4a92e7d3da1dd
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-11 13:46:46 +0200

    Add firebase to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyBe74nyu2lz-9o1H0_CnHmHd0kCOczd_nw',
+			authDomain: 'auth-95faf.firebaseapp.com',
+			databaseURL: 'https://auth-95faf.firebaseio.com',
+			projectId: 'auth-95faf',
+			storageBucket: 'auth-95faf.appspot.com',
+			messagingSenderId: '924318516662'
+		});
+	}
+
 	render() {
 		return (
 			<View>
