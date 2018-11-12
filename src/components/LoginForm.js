78814806720216175e9db69f517f772bbf9c3d49
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-12 09:22:57 +0200

    Call firebase API and show error

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from '@firebase/app';
+import 'firebase/auth';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
 	state = {
 		email: '',
-		password: ''
+		password: '',
+		error: ''
 	};
+
+	onButtonPress() {
+		const { email, password } = this.state;
+		this.setState({ error: '' });
+
+		firebase.auth()
+			.signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				firebase.auth()
+					.createUserWithEmailAndPassword(email, password)
+					.catch(() => {
+						this.setState({ error: 'Authentication Failed.' });
+					});
+			});
+	}
 
 	render() {
 		return (
@@ -29,8 +48,12 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log in
 					</Button>
 				</CardSection>
@@ -38,5 +61,13 @@ class LoginForm extends Component {
 		);
 	}
 }
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
 
 export default LoginForm;
