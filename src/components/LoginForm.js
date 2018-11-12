ed7bda9ab1e0533a609647fedf1ca2a10ebeaee3
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-11-12 11:12:12 +0200

    Hide spinner after success or fail

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,13 +18,29 @@ class LoginForm extends Component {
 
 		firebase.auth()
 			.signInWithEmailAndPassword(email, password)
+			.then(this.onLoginSuccess.bind(this))
 			.catch(() => {
 				firebase.auth()
 					.createUserWithEmailAndPassword(email, password)
-					.catch(() => {
-						this.setState({ error: 'Authentication Failed.', loading: false });
-					});
+					.then(this.onLoginSuccess.bind(this))
+					.catch(this.onLoginFail.bind(this));
 			});
+	}
+
+	onLoginSuccess() {
+		this.setState({
+			email: '',
+			password: '',
+			error: '',
+			loading: false
+		});
+	}
+
+	onLoginFail() {
+		this.setState({
+			error: 'Authentication Failed.',
+			loading: false
+		});
 	}
 
 	renderError() {
